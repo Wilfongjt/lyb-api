7c48639a7a715a9b53851ae662e863a43be4800a
James <wilfongjt@gmail.com>
2021-08-05 06:47:43 -0400

    04_api_signup

--- test/environment.test.js
+++ test/environment.test.js
@@ -7,18 +7,39 @@ const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
 
 describe('Environment', () => {
   // Initialize
-  it('Environment ', () => {
+  it('Environment NODE_ENV', () => {
 
     expect(process.env.NODE_ENV).to.exists();
+
+  })
+  it('Environment JWT_CLAIMS', () => {
+
     expect(process.env.JWT_CLAIMS).to.exists();
+
+  })
+  it('Environment JWT_SECRET', () => {
+
     expect(process.env.JWT_SECRET).to.exists();
+
+  })
+  it('Environment API_TOKEN', () => {
+
     expect(process.env.API_TOKEN).to.exists();
+
+  })
+  it('Environment HOST', () => {
+
     expect(process.env.HOST).to.exists();
+    
+  })
+  it('DATABASE_URL DATABASE_URL', () => {
+
     expect(process.env.DATABASE_URL).to.exists();
     
 
   })
-  it('Environment Types ', () => {
+
+  it('Environment Types JWT_CLAIMS', () => {
 
     expect(typeof(process.env.JWT_CLAIMS)).to.equal('string');
     
